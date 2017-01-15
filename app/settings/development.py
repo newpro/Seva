80de1a09d9adf00823394df72d224b601adda0b6
newpro <aaron.li1993@gmail.com>
2017-01-15 14:35:07 -0500

    change settings from messy to json

--- app/settings/development.py
+++ app/settings/development.py
@@ -1,14 +1,17 @@
 from basic import Config
-import secret as sec
+from secret.secrets import remote as sec
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SECRET_KEY = sec.local_secret
     SQLALCHEMY_ECHO = True # Log any sqlalchemy error
-    SQLALCHEMY_DATABASE_URI = ('postgresql://{}:{}@{}/{}').format(sec.local_db_user,
-                                                                  sec.local_db_password,
-                                                                  sec.local_db_location,
-                                                                  sec.local_db_name)
-    REALTIME_URL = sec.local_realtime_url
-    REALTIME_CRED_PATH = sec.local_realtime_cred_path
-    REALTIME_KEY = sec.local_realtime_api_key
+    SECRET_KEY = sec['secret']
+    if sec['db']['url']:
+        SQLALCHEMY_DATABASE_URI = sec['db']['url']
+    else:
+        SQLALCHEMY_DATABASE_URI = ('postgresql://{}:{}@{}/{}').format(sec['db']['user'],
+                                                                      sec['db']['password'],
+                                                                      sec['db']['location'],
+                                                                      sec['db']['name'])
+    REALTIME_URL = sec['realtime']['url']
+    REALTIME_CRED_PATH = sec['realtime']['cred_path']
+    REALTIME_KEY = sec['realtime']['api_key']
